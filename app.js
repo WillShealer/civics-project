df5cffc7f7bd65991b2d602deb1ffd78d30b9de7
Tsiegmund <90394665+Tsiegmund@users.noreply.github.com>
2022-04-05 12:02:51 -0400

    lol

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const html = require('html');
+const consolidate = require('consolidate');
 app = express()
 
 app.set('view engine', 'ejs')
 
+app.use('/static', express.static('static'))
+
 app.get('/', (req,res) => {
-  res.render('../views/home.ejs')
+  res.render('home.ejs')
 })
 
 app.listen(1234)
